ec6089a15c44890f50b7235081dbe92c79878cef
alebusi <allebusi@gmail.com>
2019-03-20 11:02:00 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,11 +1,16 @@
 function myfunc() {
-  var valore = document.myForm.myField.value;
+  var valore = 2000;
   avvisa();
-  document.getElementById("msg").className="on";
-  setInterval(avvisa, valore*60000);
+  setInterval(avvisa, valore);
 }
 
 function avvisa() {
   var suona = new Audio('timer.mp3');
+  if (document.getElementById("target").className=="bianca") {
+    document.getElementById("target").className="nera";
+  }
+  else {
+    document.getElementById("target").className="bianca";
+  }
   suona.play();
 }
