a371cdc44f4d44efd7dd6969150f64a39aff7005
alebusi <allebusi@gmail.com>
2019-12-29 16:10:04 +0100

    Update lettera.js

--- lettera.js
+++ lettera.js
@@ -1,12 +1,20 @@
 var lettersC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var timerAttivo = "N";
 
 function randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 function scriviLetteraTimer() {
-  /* mySel(); */
-  myTimer=setInterval(scriviLettera, 5000);
+  if (timerAttivo == "N") {
+    scriviLettera();
+    myTimer=setInterval(scriviLettera, 5000);
+    timerAttivo="S";
+  }
+  else {
+    clearInterval(myTimer);
+    timerAttivo="N";
+  }
 }
 
 function scriviLettera() {
