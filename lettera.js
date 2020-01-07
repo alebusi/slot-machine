606ad81322e11bb521de4e854d795a0d398c4fbe
alebusi <allebusi@gmail.com>
2020-01-07 11:41:39 +0100

    Update lettera.js

--- lettera.js
+++ lettera.js
@@ -1,5 +1,9 @@
 var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var dimensione = ["80px","120px","160px","200px","240px"];
+var peso = ["200","300","400","500","600","700","800","900"];
 var timerAttivo = "N";
+var ind = 1;
+var ind_p = 4;
 
 function randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
@@ -28,4 +32,14 @@ function cambioOpac() {
   else {
     document.getElementById("bordo").style.opacity=0;  
   }
+}
+function cambiaDimensione() {
+  ind+=1;
+  if (ind > dimensione.length-1) ind=0;
+  document.getElementById("lettera").style.fontSize=dimensione[ind];
+}
+function cambiaPeso() {
+  ind_p+=1;
+  if (ind_p > peso.length-1) ind_p=0;
+  document.getElementById("lettera").style.fontWeight=peso[ind];
 }
