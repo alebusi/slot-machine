fa6e4284c7a53f41738c523260b5a9444c4dceba
alebusi <allebusi@gmail.com>
2020-01-21 17:12:27 +0100

    Update slot.js

--- slot.js
+++ slot.js
@@ -1,4 +1,6 @@
-var colori = ["white","orange","#ff3300","#00FFFF","#FF00FF","#00C200","black"];
+var colori = ["white","orange","#ff3300","#00FFFF","#FF00FF","#00C200"];
+var colonne = ["S","S","S"];
+var colTimer = 0;
 var timerAttivo = "N";
 
 function randomIntFromInterval(min,max) {
@@ -6,9 +8,13 @@ function randomIntFromInterval(min,max) {
 }
 
 function tiraLevaTimer() {
+  colTimer=0;
+  colonne[0]="S";
+  colonne[1]="S";
+  colonne[2]="S";
   if (timerAttivo == "N") {
     tiraLeva();
-    myTimer=setInterval(tiraLeva, 5000);
+    myTimer=setInterval(tiraLeva, 500);
     timerAttivo="S";
   }
   else {
@@ -18,29 +24,40 @@ function tiraLevaTimer() {
 }
 
 function tiraLeva() {
-  document.getElementById("primo").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
-  document.getElementById("secondo").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
-  document.getElementById("terzo").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
-  document.getElementById("quarto").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
-
-  num=randomIntFromInterval(0,1);
-  if (num == 1) {
-    document.getElementById("primo").style.borderLeftColor = "transparent";
-    document.getElementById("primo").style.borderTopColor = "transparent";
+  colTimer+=1;
+  if (colTimer == 6) {
+    colonne[0]="N";
+    }
+  if (colTimer == 12) {
+    colonne[1]="N";
+    }
+  if (colTimer == 20) {
+    colonne[2]="N";
+    clearInterval(myTimer);
+    timerAttivo="N";
+    }
+  if (colonne[0]=="S") {
+    document.getElementById("primo").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
+    num=randomIntFromInterval(0,1);
+    if (num == 1) {
+      document.getElementById("primo").style.borderLeftColor = "transparent";
+      document.getElementById("primo").style.borderTopColor = "transparent";
+      }
   }
-  num=randomIntFromInterval(0,1);
-  if (num == 1) {
-    document.getElementById("secondo").style.borderRightColor = "transparent";
-    document.getElementById("secondo").style.borderTopColor = "transparent";
+  if (colonne[1]=="S") {
+    document.getElementById("secondo").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
+    num=randomIntFromInterval(0,1);
+    if (num == 1) {
+      document.getElementById("secondo").style.borderRightColor = "transparent";
+      document.getElementById("secondo").style.borderTopColor = "transparent";
+    }
   }
-  num=randomIntFromInterval(0,1);
-  if (num == 1) {
-    document.getElementById("terzo").style.borderLeftColor = "transparent";
-    document.getElementById("terzo").style.borderBottomColor = "transparent";
-  }
-  num=randomIntFromInterval(0,1);
-  if (num == 1) {
-    document.getElementById("quarto").style.borderRightColor = "transparent";
-    document.getElementById("quarto").style.borderBottomColor = "transparent";
+  if (colonne[2]=="S") {
+    document.getElementById("terzo").style.borderColor = colori[(randomIntFromInterval(0,colori.length - 1))];
+    num=randomIntFromInterval(0,1);
+    if (num == 1) {
+      document.getElementById("terzo").style.borderLeftColor = "transparent";
+      document.getElementById("terzo").style.borderBottomColor = "transparent";
+    }
   }
 }
